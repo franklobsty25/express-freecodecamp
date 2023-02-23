74269186defc442d4130ccabf2432e4ef9e79ca3
Frank Adu Opoku Kodie <frankkodie@yahoo.com>
2023-02-23 22:03:27 +0000

    time

--- myApp.js
+++ myApp.js
@@ -4,22 +4,31 @@ require('dotenv').config();
 
 app.use('/public', express.static(__dirname + '/public'));
 
-app.use(function(req, res, next) {
-    console.log(`${req.method} ${req.path} - ${req.ip} `);
-    next();
-})
+app.use(function (req, res, next) {
+  console.log(`${req.method} ${req.path} - ${req.ip} `);
+  next();
+});
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 app.get('/json', function (req, res) {
-  
   if (process.env.MESSAGE_STYLE === 'uppercase') {
     res.json({ message: 'Hello json'.toUpperCase() });
   } else {
     res.json({ message: 'Hello json' });
   }
 });
+
+app.get(
+  '/now',
+  function (req, res, next) {
+    req.time = new Date().toString();
+  },
+  function (req, res) {
+    res.json({ time: req.time });
+  }
+);
 
 module.exports = app;
