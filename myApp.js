b2d0176ac8b096e2ae60598b38d4597c9fea180d
Frank Adu Opoku Kodie <frankkodie@yahoo.com>
2023-02-23 21:39:08 +0000

    changes on env

--- myApp.js
+++ myApp.js
@@ -9,11 +9,12 @@ app.get('/', function (req, res) {
 });
 
 app.get('/json', function (req, res) {
-  var msg =
-    process.env.MESSAGE_STYLE === 'uppercase'
-      ? 'Hello json'.toUpperCase()
-      : 'Hello json';
-  res.json({ message: msg });
+  console.log(process.env.MESSAGE_STYLE === 'uppercase');
+  if (process.env.MESSAGE_STYLE === 'uppercase') {
+    res.json({ message: 'Hello json'.toUpperCase() });
+  } else {
+    res.json({ message: 'Hello json' });
+  }
 });
 
 module.exports = app;
