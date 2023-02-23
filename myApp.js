4f07a64ba21a1defc747669cb918091eb3b6be87
Frank Adu Opoku Kodie <frankkodie@yahoo.com>
2023-02-23 21:55:25 +0000

    middleware

--- myApp.js
+++ myApp.js
@@ -4,12 +4,17 @@ require('dotenv').config();
 
 app.use('/public', express.static(__dirname + '/public'));
 
+app.use(function(req, res, next) {
+    console.log(`${req.method} ${req.path} - ${req.ip} `);
+    next();
+})
+
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 app.get('/json', function (req, res) {
-  console.log(process.env.MESSAGE_STYLE === 'uppercase');
+  
   if (process.env.MESSAGE_STYLE === 'uppercase') {
     res.json({ message: 'Hello json'.toUpperCase() });
   } else {
