4ad12c52072235664e05686584dacf8f6b97b36c
Frank Adu Opoku Kodie <frankkodie@yahoo.com>
2023-02-23 20:51:31 +0000

    json added

--- myApp.js
+++ myApp.js
@@ -3,43 +3,12 @@ let app = express();
 
 app.use('/public', express.static(__dirname + '/public'));
 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/views/index.html');
-})
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
 
+app.get('/json', function (req, res) {
+  res.json({ message: 'Hello json' });
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
