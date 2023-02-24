c9e539f2bd59a926e26cc3dbc24cb3701918c9ef
Frank Adu Opoku Kodie <frankkodie@yahoo.com>
2023-02-24 10:48:12 +0000

    body-parser

--- myApp.js
+++ myApp.js
@@ -1,6 +1,9 @@
 let express = require('express');
+let bodyParser = require('body-parser');
 let app = express();
 require('dotenv').config();
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/public', express.static(__dirname + '/public'));
 
